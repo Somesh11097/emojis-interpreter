06e42537d8795756fca28e5a502645127c598c92
Somesh banerjee <32162479+Somesh11097@users.noreply.github.com>
2021-01-14 12:53:58 +0530

    doc:updated placeholder

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ var emojiDictionary = {
   "🤑": "Rich",
   "🥵": "Overheated Face"
 };
+var emojisWeKnow = Object.keys(emojiDictionary);
 
 export default function App() {
   const [meaning, setMeaning] = useState(" ");
@@ -23,14 +24,32 @@ export default function App() {
     // console.log(meaning);
     setMeaning(meaning);
   }
+
+  function emojiClickedHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    setMeaning(meaning);
+  }
   return (
     <div className="App">
       <h1>🙂 Emoticons 🙃</h1>
       <input
-        placeholder="Enter Something to know the meaning of"
+        placeholder="Enter Emoji  to know the meaning "
         onChange={emojiInputHandler}
       ></input>
       <h2> {meaning} </h2>
+      <h3>Emojis we Know </h3>
+      {emojisWeKnow.map((emoji) => {
+        return (
+          <span
+            onClick={() => emojiClickedHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {" "}
+            {emoji}{" "}
+          </span>
+        );
+      })}
     </div>
   );
 }
